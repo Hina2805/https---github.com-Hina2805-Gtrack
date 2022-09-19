ac10fddb2074b39cf655d0b39c41acb74c4a0066
Shoaib Ahmed <shoaib.ahmed@seatech.com>
2022-09-19 12:33:18 +0500

    login test case

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,19 @@
+import 'cypress-xpath'
+describe('Login', () => {
+  it('passes', () => {
+    cy
+    .viewport(1920, 1080)
+    cy
+    .visit('https://app-dev.gtrackdev.top/')
+    cy
+    .xpath('//input[@placeholder="Username"]')
+    .type("Alice")
+    cy.
+    xpath('//input[@placeholder="Enter Password"]')
+    .type("P@ssword123")
+    cy
+    .xpath('//button[@name="button" and @value="login"]')
+    .click()
+
+  })
+})
