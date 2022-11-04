f3f9ac89eb2d2cfc9edd0ee567a51a027f2fba30
Shoaib Ahmed <shoaib.ahmed@seatech.com>
2022-11-04 11:34:46 +0500

    updated external party

--- cypress/e2e/Gtrack.cy.ts
+++ cypress/e2e/Gtrack.cy.ts
@@ -1,9 +1,11 @@
 import 'cypress-xpath'
 import { faker } from '@faker-js/faker';
-import { parseIsolatedEntityName } from 'typescript'
 import{LoginPage} from "../pages/login_page"
 describe('Gtrack', () => {
   const loginpage= new LoginPage()
+  const faker = require('faker')
+  EPname: faker.name.findName()
+
   beforeEach(()=>{
     cy.viewport(1920, 1080)
     cy.visit('https://app-dev.gtrackdev.top/')
@@ -15,19 +17,58 @@ describe('Gtrack', () => {
 })
  it('External Party', function(){
  cy
- .xpath('//a[@href="/external-party/list"]')
+ .get('.gt-header-wrapper .gt-header-menu .p-menubar [role="menubar"] li:nth-child(4) .p-submenu-list li:nth-child(1) .p-menuitem-link .p-menuitem-text')
  .click({force:true})
  cy
- .xpath('//p-button[@label="Add External Party"]')
+ .get('trk-external-party-list .gt-filter-btn-wrapper p-button span:nth-child(2)')
  .click({force:true})
  cy
  .get('[formcontrolname="externalPartyType"] .p-dropdown > .p-inputtext')
  .click()
  cy
- .xpath('//li[@role="option" and  @aria-label="Producer"]')
+ .get('trk-upsert-external-party [formcontrolname="externalPartyType"] [role="listbox"] [aria-label="Producer"]')
  .click()
  cy
- .xpath('//input[@formcontrolname="name" and  @fieldname="externalPartyName"]')
- .type()
+ .get('.gt-card-container div:nth-child(2) [fieldname="externalPartyName"]')
+ .type(this.EPname)
+ cy
+ .get('.gt-card-container div:nth-child(3) [formcontrolname="email"]')
+ .type('c1@mail.com')
+ cy
+ .get('.gt-card-container div:nth-child(4) [formcontrolname="phone"]')
+ .type('03001234567')
+ cy
+ .get('.gt-card-container div:nth-child(5) [formcontrolname="address1"]')
+ .type('address 1 abc')
+ cy
+ .get('.gt-card-container div:nth-child(6) [formcontrolname="address2"]')
+ .type('adddress 2 xyz')
+ cy
+ .get('[formcontrolname="countryId"] .p-dropdown > .p-inputtext')
+ .click()
+ cy
+ .get('[formcontrolname="countryId"] .p-dropdown .p-dropdown-items-wrapper .p-element [aria-label="Pakistan"]')
+ .click()
+ cy
+ .get('[formcontrolname="provinceId"] .p-dropdown > .p-inputtext')
+ .click()
+ cy
+ .get('[formcontrolname="provinceId"] .p-dropdown .p-dropdown-items-wrapper .p-element [aria-label="Azad Kashmir"]')
+ .click()
+ cy
+ .get('[formcontrolname="cityId"] .p-dropdown > .p-inputtext')
+ .click()
+ cy
+ .get('[formcontrolname="cityId"] .p-dropdown .p-dropdown-items-wrapper .p-element [aria-label="Bhimbar"]')
+ .click()
+ cy
+ .get('.gt-card-container div:nth-child(10) [formcontrolname="website"]')
+ .type('www.abc.com')
+ cy
+.get('trk-upsert-external-party .gt-btns-wrapper .gt-ctm-btn')
+.click()
+ })
+ it('Add Plant/Branch', function(){
+
  })
 })
--- cypress/pages/login_page.ts
+++ cypress/pages/login_page.ts
@@ -1,13 +1,13 @@
 import  'cypress-xpath'
 export class LoginPage{
 
-    loginPage_username = '//input[@id="Username"]'
+    loginPage_username = '#Username'
     loginPage_password ='//input[@placeholder="Enter Password"]'
     loginPage_loginButton ='//button[@name="button" and @value="login"]'
     homePageExternalPartiesList = '//a[@href="/external-Party/contact/list"]'
     
     enterUsername(username: string){
-        cy.xpath(this.loginPage_username).type(username)
+        cy.get(this.loginPage_username).type(username)
 
     }
     enterPassword(password: string){
